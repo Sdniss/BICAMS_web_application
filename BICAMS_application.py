1b330b3e7614a39784dae44469278366faafa5df
Stijn Denissen <40318470+Sdniss@users.noreply.github.com>
2020-11-10 09:46:10 +0100

    Update BICAMS_application.py
    Add .

--- BICAMS_application.py
+++ BICAMS_application.py
@@ -150,7 +150,7 @@ show_disclaimer = st.button(label = 'Show disclaimer about impairment cut-off of
 if show_disclaimer:
             st.write('*The sensitivity of this normalization method (correcting with regression-based norms from a healthy reference population) is higher '
                      'than traditional normalization methods. Thus, it is recommended to use a lower cut-off of -1 or -0.5, opposed to the [traditional value of -1.5](https://n.neurology.org/content/90/6/278), '
-                     'to classify a subject as cognitively impaired*')
+                     'to classify a subject as cognitively impaired.*')
 st.write('This figure shows a gaussian, a distribution that is very common in nature. For example, **length** follows a gaussian distribution.') 
 st.subheader('Figure interpretation')
 st.write('- Many people will have an average length, so the curve is high in the middle')
